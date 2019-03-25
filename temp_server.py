f27fe79213ce0486cf04f169ea6b00a382cc433e
mstumbo <mattstumbo@icloud.com>
2019-03-25 08:37:47 -0700

    initial commit

---
+++ temp_server.py
@@ -0,0 +1,43 @@
+import json
+import time
+import readadc
+import datetime
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+# temperature sensor middle pin connected channel 0 of mcp3008
+sensor_pin = 0
+readadc.initialize()
+
+#the main sensor reading and plotting loop
+def read_tmp36():
+    sensor_data = readadc.readadc(sensor_pin,
+                                  readadc.PINS.SPICLK,
+                                  readadc.PINS.SPIMOSI,
+                                  readadc.PINS.SPIMISO,
+                                  readadc.PINS.SPICS)
+
+    millivolts = sensor_data * (3300.0 / 1024.0)
+    # 10 mv per degree
+    temp_C = ((millivolts - 100.0) / 10.0) - 40.0
+    # convert celsius to fahrenheit
+    temp_F = (temp_C * 9.0 / 5.0) + 32
+    # remove decimal point from millivolts
+    millivolts = "%d" % millivolts
+    # show only one decimal place for temprature and voltage readings
+    temp_C = "%.1f" % temp_C
+    temp_F = "%.1f" % temp_F
+
+    # return the data
+    return temp_C
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(str.encode('{"value": %s}'%read_tmp36()))
+
+httpd = HTTPServer(('0.0.0.0', 80), SimpleHTTPRequestHandler)
+httpd.serve_forever()
+
---
+++ tmp36.py
@@ -0,0 +1,36 @@
+import json
+import time
+import readadc
+import datetime
+
+
+# temperature sensor middle pin connected channel 0 of mcp3008
+sensor_pin = 0
+readadc.initialize()
+
+#the main sensor reading and plotting loop
+while True:
+    sensor_data = readadc.readadc(sensor_pin,
+                                  readadc.PINS.SPICLK,
+                                  readadc.PINS.SPIMOSI,
+                                  readadc.PINS.SPIMISO,
+                                  readadc.PINS.SPICS)
+
+    millivolts = sensor_data * (3300.0 / 1024.0)
+    print(millivolts)
+    # 10 mv per degree
+    temp_C = ((millivolts - 100.0) / 10.0) - 40.0
+    # convert celsius to fahrenheit
+    temp_F = (temp_C * 9.0 / 5.0) + 32
+    # remove decimal point from millivolts
+    millivolts = "%d" % millivolts
+    # show only one decimal place for temprature and voltage readings
+    temp_C = "%.1f" % temp_C
+    temp_F = "%.1f" % temp_F
+
+    # write the data to plotly
+    print(temp_F)
+    print(temp_C)
+    print()
+    # delay between stream posts
+    time.sleep(0.25)
